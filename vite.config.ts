978ee2d2cf6710d37c6fd752482483d78ed7500f
lutz <luocong2016@126.com>
2024-02-21 14:53:00 +0800

    chore: vite.conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,29 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    // jsxInject: "import { h } from 'vue';",
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment'
+  },
+  server: {
+    port: 5173,
+    host: true,
+    proxy: {}
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
   plugins: [
     vue(),
     vueJsx({
-      // options are passed on to @vue/babel-plugin-jsx
+      /* options are passed on to @vue/babel-plugin-jsx */
     })
   ]
 })
