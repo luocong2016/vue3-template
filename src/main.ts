f200e850a043f8a55c12c87a742e10bac199c809
lutz <luocong2016@126.com>
2024-02-27 16:41:09 +0800

    feat: pinia 持久化

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPersist from 'pinia-plugin-persist'
 
 import 'ant-design-vue/dist/reset.css'
 import './style.less'
 import App from './App.vue'
 
 const pinia = createPinia()
+pinia.use(piniaPersist)
+
 const app = createApp(App)
 
 app.use(pinia)
